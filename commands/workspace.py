c81d1cd2239c5eb558d389d036ff18cc546731fa
Korvedae <korvedae@proton.me>
2025-05-09 15:22:01 -0500

    refactor: 5 workspaces now

--- commands/workspace.py
+++ commands/workspace.py
@@ -8,16 +8,32 @@ def _run(command):
 
 	match command[1]:
 		case "1":
-			spcommand._send_hypr_command("dispatch moveworkspacetomonitor 1001 HDMI-A-2")
-			spcommand._send_hypr_command("dispatch moveworkspacetomonitor 1002 DP-1")
-			spcommand._send_hypr_command("dispatch moveworkspacetomonitor 1003 HDMI-A-1")
+			test = spcommand._send_hypr_command('cursorpos')
+			print(test)
+			print(test)
+			print(test)
+			print(test)
+			print(test)
+			print(test)
+			print(test)
+			spcommand._send_hypr_command("dispatch workspace 1001")
+			spcommand._send_hypr_command("dispatch workspace 1002")
+			spcommand._send_hypr_command("dispatch workspace 1003")
 		case "2":
-			spcommand._send_hypr_command("dispatch moveworkspacetomonitor 1011 HDMI-A-2")
-			spcommand._send_hypr_command("dispatch moveworkspacetomonitor 1012 DP-1")
-			spcommand._send_hypr_command("dispatch moveworkspacetomonitor 1013 HDMI-A-1")
+			spcommand._send_hypr_command("dispatch workspace 1011")
+			spcommand._send_hypr_command("dispatch workspace 1012")
+			spcommand._send_hypr_command("dispatch workspace 1013")
 		case "3":
-			spcommand._send_hypr_command("dispatch moveworkspacetomonitor 1011 HDMI-A-2")
-			spcommand._send_hypr_command("dispatch moveworkspacetomonitor 1012 DP-1")
-			spcommand._send_hypr_command("dispatch moveworkspacetomonitor 1013 HDMI-A-1")
+			spcommand._send_hypr_command("dispatch workspace 1021")
+			spcommand._send_hypr_command("dispatch workspace 1022")
+			spcommand._send_hypr_command("dispatch workspace 1023")
+		case "4":
+			spcommand._send_hypr_command("dispatch workspace 1031")
+			spcommand._send_hypr_command("dispatch workspace 1032")
+			spcommand._send_hypr_command("dispatch workspace 1033")
+		case "5":
+			spcommand._send_hypr_command("dispatch workspace 1041")
+			spcommand._send_hypr_command("dispatch workspace 1042")
+			spcommand._send_hypr_command("dispatch workspace 1043")
 		case _:
-			print("This only supports 3 moveworkspacetomonitors for now")
+			print("This only supports 5 workspaces for now")
