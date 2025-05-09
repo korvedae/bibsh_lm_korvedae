2bab62e23ecb081aca4f5d3f19740e24cbb3e3db
Korvedae <korvedae@proton.me>
2025-05-08 22:40:22 -0500

    feat(breaking): Added client socket and listener

--- main.py
+++ main.py
@@ -19,20 +19,40 @@ base_path = os.path.join(xdg_runtime_dir, "hypr", hypr_sig)
 event_socket_path = os.path.join(base_path, ".socket2.sock")
 command_socket_path = os.path.join(base_path, ".socket.sock")
 
-
-print(command_socket_path)
+client_socket_path = f'{os.environ.get("XDG_RUNTIME_DIR")}/hypr_span.client.sock'
 
 
 # Listener for Hyprland events
 def listen_to_events():
-    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
-        sock.connect(event_socket_path)
-        while True:
-            data = sock.recv(4096)
-            if not data:
-                break
-            event_handler(data.decode().strip())
-            print("Event:", data.decode().strip())
+	with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+		sock.connect(event_socket_path)
+		while True:
+			data = sock.recv(4096)
+			if not data:
+				break
+			event_handler(data.decode().strip())
+			print("Event:", data.decode().strip())
+
+def _client_socket():
+	with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+		try:
+			os.unlink(client_socket_path)
+		except OSError:
+			if os.path.exists(client_socket_path):
+				raise
+		sock.bind(client_socket_path)
+		sock.listen(1)
+		print(f'Client Socket Opened at {client_socket_path}')
+
+
+def listen_for_client():
+	with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+		sock.connect(client_socket_path)
+		while True:
+			data = sock.recv(4096)
+			if not data:
+				break
+			client_handler(data.decode().strip())
 
 # Send a command to Hyprland
 def send_command(command: str):
@@ -46,11 +66,22 @@ def event_handler(event: str):
 	event_keyword = event.rstrip('>>')
 	print(f'keyword parsed: {event_keyword}')
 
+def client_handler(command: str):
+	print(command)
+	pass
+
 
 # Run event listener in a thread
 event_thread = threading.Thread(target=listen_to_events, daemon=True)
 event_thread.start()
 
+# Create the client socket
+client_socket = threading.Thread(target=_client_socket, daemon=True)
+client_socket.start()
+
+# Bind the socket to the path
+client_thread = threading.Thread(target=listen_for_client, daemon=True)
+client_thread.start()
 
 # Example: Send a test command
 send_command("dispatch workspace 1")
