cbd03511fa72a019737686733bb5b30452b8e940
Korvedae <korvedae@proton.me>
2025-05-10 22:32:00 -0500

    docs: Updating spconfig.json to match README. Added mask to documentation as well.

--- commands/workspace.py
+++ commands/workspace.py
@@ -1,4 +1,5 @@
 import commands.SPCommand as spcommand
+import settings
 
 def _run(command):
 	try:
@@ -8,8 +9,35 @@ def _run(command):
 
 	old_position = ''.join(spcommand._send_hypr_command("cursorpos")).replace(" ", '').split(',')
 
+	# For determining iterator
+	monitor_iterator = 1
+	workspace_iterator  = 1
+
+	mask_length = len(settings.spconfig['workspace']['mask'])
+	for char_pos in range(mask_length):
+		if settings.spconfig['workspace']['mask'][char_pos] == "w":
+			# I wanted to do this cleanly using math and powers and stuff
+			# But i'm not smart enough for that so this will have to do.
+			workspace_iterator = (mask_length - 1) - char_pos
+
+			for thingy in range(workspace_iterator):
+				workspace_iterator = str(workspace_iterator)
+				workspace_iterator = workspace_iterator + '0'
+
+			print(workspace_iterator)
+
+
+
+			# Note: Trying to convert 3 - 2 to 10... somehow
+
+		elif settings.spconfig['workspace']['mask'][char_pos] == "m":
+			pass
+
+
+
 	match command[1]:
 		case "1":
+			spcommand._send_hypr_command("dispatch workspace 1001")
 			spcommand._send_hypr_command("dispatch workspace 1001")
 			spcommand._send_hypr_command("dispatch workspace 1002")
 			spcommand._send_hypr_command("dispatch workspace 1003")
