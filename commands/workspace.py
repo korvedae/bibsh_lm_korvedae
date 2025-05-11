0440490a08289e0f7deecfd73733086adc407856
Korvedae <korvedae@proton.me>
2025-05-10 23:42:51 -0500

    refactor: Workspaces are auto determined based on monitor count. Additionally, I also had to swap the monitor index and workspace parameters. This was to fix a bug where hyprland sometimes wouldn't change the workspace. Which was probably due to it having to move across too many workspaces.

--- commands/workspace.py
+++ commands/workspace.py
@@ -1,5 +1,6 @@
 import commands.SPCommand as spcommand
 import settings
+import subprocess
 
 def _run(command):
 	try:
@@ -9,55 +10,19 @@ def _run(command):
 
 	old_position = ''.join(spcommand._send_hypr_command("cursorpos")).replace(" ", '').split(',')
 
-	# For determining iterator
-	monitor_iterator = 1
-	workspace_iterator  = 1
+	# Only allow 10 spanned workspaces
+	if int(command[1]) > 9:
+		print("Parameter Error: Only 10 workspaces are allowed")
+		return
+	if len(settings.spconfig['monitors']) > 9:
+		print("Config Error: Only 9 monitors max.")
+		return
 
-	mask_length = len(settings.spconfig['workspace']['mask'])
-	for char_pos in range(mask_length):
-		if settings.spconfig['workspace']['mask'][char_pos] == "w":
-			# I wanted to do this cleanly using math and powers and stuff
-			# But i'm not smart enough for that so this will have to do.
-			workspace_iterator = (mask_length - 1) - char_pos
+	print(f'For workspace {command[1]}')
+	for x in range(len(settings.spconfig['monitors'])):
 
-			for thingy in range(workspace_iterator):
-				workspace_iterator = str(workspace_iterator)
-				workspace_iterator = workspace_iterator + '0'
-
-			print(workspace_iterator)
-
-
-
-			# Note: Trying to convert 3 - 2 to 10... somehow
-
-		elif settings.spconfig['workspace']['mask'][char_pos] == "m":
-			pass
-
-
-
-	match command[1]:
-		case "1":
-			spcommand._send_hypr_command("dispatch workspace 1001")
-			spcommand._send_hypr_command("dispatch workspace 1001")
-			spcommand._send_hypr_command("dispatch workspace 1002")
-			spcommand._send_hypr_command("dispatch workspace 1003")
-		case "2":
-			spcommand._send_hypr_command("dispatch workspace 1011")
-			spcommand._send_hypr_command("dispatch workspace 1012")
-			spcommand._send_hypr_command("dispatch workspace 1013")
-		case "3":
-			spcommand._send_hypr_command("dispatch workspace 1021")
-			spcommand._send_hypr_command("dispatch workspace 1022")
-			spcommand._send_hypr_command("dispatch workspace 1023")
-		case "4":
-			spcommand._send_hypr_command("dispatch workspace 1031")
-			spcommand._send_hypr_command("dispatch workspace 1032")
-			spcommand._send_hypr_command("dispatch workspace 1033")
-		case "5":
-			spcommand._send_hypr_command("dispatch workspace 1041")
-			spcommand._send_hypr_command("dispatch workspace 1042")
-			spcommand._send_hypr_command("dispatch workspace 1043")
-		case _:
-			print("This only supports 5 workspaces for now")
+		print(f'monitor "{settings.spconfig['monitors'][x]}" subspace 10{command[1]}{x + 1} on workspace {command[1]}')
+		print(spcommand._send_hypr_command(f'dispatch focusmonitor {settings.spconfig['monitors'][x]}'))
+		print(spcommand._send_hypr_command(f'dispatch workspace 10{x + 1}{command[1]}'))
 
 	spcommand._send_hypr_command(f'dispatch movecursor {old_position[0]} {old_position[1]}')
