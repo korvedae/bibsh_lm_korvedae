584845cb5ba7d788a6eabdcea4a049e9f0242f90
Korvedae <korvedae@proton.me>
2025-05-23 08:33:37 -0500

    Workspaces now iterate by number in config file

--- commands/workspace.py
+++ commands/workspace.py
@@ -10,8 +10,8 @@ def _run(command):
 	old_position = ''.join(spcommand._send_hypr_command("cursorpos")).replace(" ", '').split(',')
 
 	# Only allow 10 spanned workspaces
-	if int(command[1]) > 9:
-		print("Parameter Error: Only 10 workspaces are allowed")
+	if int(command[1]) > settings.spconfig['workspacesPerMonitor']:
+		print(f"Parameter Error: Config file defines only {settings.spconfig['workspacesPerMonitor']} workspaces per monitor.")
 		return
 	if len(settings.spconfig['monitors']) > 9:
 		print("Config Error: Only 9 monitors max.")
@@ -19,8 +19,9 @@ def _run(command):
 
 	print(f'For workspace {command[1]}')
 	for x in range(len(settings.spconfig['monitors'])):
-
+		y = (x + 1) * 5
+		workspace_num = (y - 5) + int(command[1])
 		spcommand._send_hypr_command(f'dispatch focusmonitor {settings.spconfig['monitors'][x]}')
-		spcommand._send_hypr_command(f'dispatch workspace 10{x + 1}{command[1]}')
+		spcommand._send_hypr_command(f'dispatch workspace {workspace_num}')
 
 	spcommand._send_hypr_command(f'dispatch movecursor {old_position[0]} {old_position[1]}')
